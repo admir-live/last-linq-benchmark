76b6516f3a38999977a647cc4a1093feb6db890d
Admir Mujkic <hello@admirmujkic.com>
2024-04-14 22:18:19 +0200

    Init

---
+++ LINQPerformanceBenchmarks/PeopleDataBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace LINQPerformanceBenchmarks;
+
+public class PeopleDataBenchmark
+{
+    private List<Person>? _peopleList;
+
+    [GlobalSetup]
+    public void InitializePeopleList()
+    {
+        _peopleList = new List<Person>();
+        for (var index = 0; index < 10000; index++)
+        {
+            _peopleList.Add(new Person { Name = $"Person{index}", Age = index });
+        }
+    }
+
+    [Benchmark]
+    public void FetchLastPersonUsingLinq() 
+    {
+        var lastPerson = _peopleList!.Last();
+    }
+
+    [Benchmark]
+    public void FetchLastPersonUsingIndex()
+    {
+        var lastPerson = _peopleList![^1];
+    }
+}
---
+++ LINQPerformanceBenchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace LINQPerformanceBenchmarks;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<PeopleDataBenchmark>();
+    }
+}
